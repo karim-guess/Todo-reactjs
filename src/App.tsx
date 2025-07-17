7c3cacff8544f17d0c713448c78f2e23b6c886d0
karim <karim.guessab@hotmail.com>
2025-07-17 20:08:38 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 
 
@@ -5,7 +6,7 @@
 
 
 
-type Priority = 'low' | 'medium' | 'high';
+type Priority = 'Urgente' | 'Moyenne' | 'Petite';
 
 type Todo = {
   id:number;
@@ -15,17 +16,39 @@ type Todo = {
 
 function App() {
 
+ const [ input , setInput ] = useState("");
+ const [ priority , setPriority ] = useState<Priority>("Moyenne");
 
   return (
-    <>
+    
       <div className="flex justify-center" >
 
-         <div className="w-2/3 flex flex-col gap-4 my-16 bg-base-300" >
+         <div className="w-2/3 flex flex-col gap-4 my-16 bg-base-300 p-5 rounded-2xl" >
+
+        <div className="flex gap-4 ">
+<input type="text"
+className="input w-full"
+ placeholder="Ajouter une tache ..."
+ value={input}
+ onChange={(e) => setInput(e.target.value)}
+ />
+<select 
+className="select w-full" value={priority}
+onChange={(e) => setPriority(e.target.value as Priority)}
+>
+<option value="Urgente">Urgente</option>
+<option value="Moyenne">Moyenne</option>
+<option value="Petite">Petite</option>
+</select>
+<button className="btn btn-primary" >
+  Ajouter
+</button>
+        </div>
 
          </div>
 
       </div>
-    </>
+   
   )
 }
 
