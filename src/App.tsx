1eac914e6dbd846126faae374ff13f8776af0eaa
karim <karim.guessab@hotmail.com>
2025-07-17 19:48:09 +0200

    First commit TodoReactjs DaisyUI

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+type Priority = 'low' | 'medium' | 'high';
+
+type Todo = {
+  id:number;
+  text:string;
+  priority :Priority;
+}
+
+function App() {
+
+
+  return (
+    <>
+      <div className="flex justify-center" >
+
+         <div className="w-2/3 flex flex-col gap-4 my-16 bg-base-300" >
+
+         </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
