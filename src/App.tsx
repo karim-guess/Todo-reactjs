7a72a702d882f1c16708143c65d13338d943d6af
karim <karim.guessab@hotmail.com>
2025-07-18 16:38:51 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { useState } from "react";
-
-
-
-
-
+import { useEffect, useState } from "react";
+import Todoitem from "./Todoitem";
 
 
 type Priority = 'Urgente' | 'Moyenne' | 'Petite';
@@ -19,6 +15,44 @@ function App() {
  const [ input , setInput ] = useState("");
  const [ priority , setPriority ] = useState<Priority>("Moyenne");
 
+ const savedTodos = localStorage.getItem("todos");
+ const initialTodos = savedTodos ? JSON.parse(savedTodos) : [];
+ const [todos, setTodos] = useState<Todo[]>(initialTodos);
+ const [filter, setFilter] = useState<Priority | "Tous">("Tous");
+
+ useEffect(() => {
+ localStorage.setItem("todos", JSON.stringify(todos));
+ }, [todos])
+
+ function addTodo() {
+  if (input.trim() == "") {
+    alert("Veuillez entrer une tâche.");
+    return;
+  }
+
+
+  const newTodo: Todo = {
+    id: Date.now(),
+    text: input,
+    priority: priority,
+  };
+
+
+
+ const  newTodos = [newTodo, ...todos];
+ setTodos(newTodos);
+  setInput("");
+  setPriority("Moyenne");
+}
+
+let filteredTodos: Todo[] = [];
+if (filter === "Tous") {
+  filteredTodos = todos;
+} else {
+  filteredTodos = todos.filter(todo => todo.priority === filter);
+}
+
+
   return (
     
       <div className="flex justify-center" >
@@ -26,8 +60,8 @@ function App() {
          <div className="w-2/3 flex flex-col gap-4 my-16 bg-base-300 p-5 rounded-2xl" >
 
         <div className="flex gap-4 ">
-<input type="text"
-className="input w-full"
+ <input type="text"
+ className="input w-full"
  placeholder="Ajouter une tache ..."
  value={input}
  onChange={(e) => setInput(e.target.value)}
@@ -40,10 +74,38 @@ onChange={(e) => setPriority(e.target.value as Priority)}
 <option value="Moyenne">Moyenne</option>
 <option value="Petite">Petite</option>
 </select>
-<button className="btn btn-primary" >
+<button onClick={addTodo} className="btn btn-primary" >
   Ajouter
 </button>
-        </div>
+        </div> 
+
+            <div className="space-y-2 flex-1 h-fit" >
+              <div className="flex flex-wrap gap-4">
+                    <button className={`btn btn-soft ${filter === "Tous" ? "btn-primary" : ""}`} onClick={() => setFilter("Tous")}   
+                    >
+                      Tous  
+                    </button>
+
+              </div>
+
+            {filteredTodos.length > 0 ? (
+              <ul className="divide-y divide-primary/20" >
+                
+              {filteredTodos.map((todo) => (
+                 
+                 <li key={todo.id} >
+                 < Todoitem todo={todo} />
+                  </li>
+              ))}
+
+                
+              </ul>
+              ) : ( 
+                <div>test 2</div>
+            )}
+
+            </div>
+
 
          </div>
 
